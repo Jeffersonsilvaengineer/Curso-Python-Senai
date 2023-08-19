4ac5c9715368ef8ed985c178175968e477be217c
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-08-19 16:40:02 -0300

    Exercicios

---
+++ Primeira Prova.py
@@ -0,0 +1,21 @@
+'''Faça um programa que apresente o menu de opções a seguir, que permita ao usuárioescolher a opção desejada,
+receba os dados necessários para executar a operação e mostre o resultado. Verificar a possibilidade de opção inválida
+ e não se preocupar com as restrições, como salário inválido.
+Menu de opções:
+
+1. Novo salário
+2. Férias
+3. Décimo terceiro
+4. Sair
+
+Digite a opção desejada :
+Na Opção 1: receber o salário de um funcionário, calcular e mostrar o novo salário usando as regras a seguir.
+Salários Percentagem de aumento: Até R$ 350,00 15%, De R$ 350,00 a R$ 600,00 10%, Acima de R$ 600,00 5%.
+
+Na opção 2: receber o salário de um funcionário, calcular e mostrar o valor de suas férias. Sabe-se que as férias
+equivalem ao seu salário acrescido de l/2.
+
+Na opção 3: receber o salário de um funcionário e o número de meses de trabalho na empresa, no máximo 12, calcular
+ e mostrar o valor do décimo terceiro. Sabe-se que o décimo terceiro equivale ao seu salário multiplicado pelo número
+ de meses de trabalho dividido por 12.
+Na opção 4: sair do programa.'''
