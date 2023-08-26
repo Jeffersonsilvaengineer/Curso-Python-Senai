5899b6e52e10d171d66b1b2cc89daa60b6dda59a
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-08-25 23:20:59 -0300

    EXERCÍCIOS

--- Aula 1 Ex 1 Básico.py
+++ Aula 1 Ex 1 Básico.py
@@ -4,6 +4,10 @@ terreno e depois exibir a área do terreno.'''
 
 largura = float(input('Digite a largura do terreno: '))
 comprimento = float(input('Digite o comprimento do terreno: '))
+while comprimento <= largura:
+    print('Desculpe está medida é inválida!')
+    largura = float(input('Digite a largura do terreno: '))
+    comprimento = float(input('Digite o comprimento do terreno: '))
 area = largura * comprimento
 print(f'A área total do terreno é {area} metros quadrados!')
 
