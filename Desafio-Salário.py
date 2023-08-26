a4ef618d59937750b61c1703fab1606e1e6e46b9
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-08-26 08:42:20 -0300

    EXERCÍCIOS

---
+++ Desafio-Salário.py
@@ -0,0 +1,11 @@
+
+salario_hr = float(input('Valor hr: '))
+hrs_mes = float(input('Horas trabalhadas no mês: '))
+
+salario = (((salario_hr * 8) * 5) * 4)
+
+if hrs_mes <= 160:
+    print('Seu salario é:', salario)
+else:
+    salario += ((salario_hr * 1.5) * (hrs_mes - 160))
+    print(salario)
