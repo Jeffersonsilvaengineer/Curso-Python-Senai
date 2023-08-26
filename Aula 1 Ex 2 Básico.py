8f9b43db58450d24bc22e8229b9ea23aa30bf989
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-08-26 14:02:01 -0300

    Exercicios

--- Aula 1 Ex 2 Básico.py
+++ Aula 1 Ex 2 Básico.py
@@ -1,6 +1,6 @@
-'''2) Um posto pensando na comodidade para seus clientes deseja disponibilizar uma busca prévia para os clientes que
-pretendem abastecer. Escreva um algoritmo para ler o preço do litro da gasolina e o valor do pagamento,
-e exibir quantos litros ele conseguiria abastecer.'''
+'''2) Um posto pensando na comodidade para seus clientes deseja disponibilizar uma busca prévia para os
+clientes que pretendem abastecer. Escreva um algoritmo para ler o preço do litro da gasolina e o valor
+do pagamento, e exibir quantos litros ele conseguiria abastecer.'''
 
 preço = float(input('Qual o preço do litro de gasolina: '))
 valor = float(input('Qual o valor a ser pago pelo abastecimento: '))
