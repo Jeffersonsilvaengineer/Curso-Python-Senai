aa63b0a38b627d055ca827f58f1b4f06f8e4bf04
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-08-26 14:02:50 -0300

    Exercicios

--- Aula 1 Ex 4 Básico.py
+++ Aula 1 Ex 4 Básico.py
@@ -1,2 +1,4 @@
-'''4) O usuário deve informar o dia, mês e ano e o sistema deve informar quantos dias se passaram desde o início do ano.
-Esqueça a questão dos anos bissextos e considere sempre que um mês possui 30 dias.'''
+'''4) O usuário deve informar o dia, mês e ano e o sistema deve informar quantos dias se passaram desde o
+início do ano. Esqueça a questão dos anos bissextos e considere sempre que um mês possui 30 dias.'''
+
+
