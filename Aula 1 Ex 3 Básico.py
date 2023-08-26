13ce18119f5f9e6c9190cddf819ab8abeeb7a3c5
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-08-26 14:02:21 -0300

    Exercicios

--- Aula 1 Ex 3 Básico.py
+++ Aula 1 Ex 3 Básico.py
@@ -1,6 +1,7 @@
 '''3) Um rancho deseja automatizar o seus pedidos de compra do mês de ração para seus cavalos. Sabendo que cada cavalo
 come por dia 1,2 Kg de ração, o usuário deve informar quantos cavalos precisam de ração e o sistema deve devolver a
 quantidade que deve ser comprada.'''
+
 dia = 1.2
 mes = 30
 cavalos = float(input('Quantos cavalos precisan de ração: '))
