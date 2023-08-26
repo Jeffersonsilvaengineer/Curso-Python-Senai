887db2ed55780cd33904b0a55bcd7fdecdc874ae
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-08-25 23:22:35 -0300

    EXERCÍCIOS

--- Aula 1 Ex 2 Básico.py
+++ Aula 1 Ex 2 Básico.py
@@ -1,4 +1,4 @@
-'''2) Um posto pensando na comodidade para seus clientes deseja disponibilizar uma busca previa para os clientes que
+'''2) Um posto pensando na comodidade para seus clientes deseja disponibilizar uma busca prévia para os clientes que
 pretendem abastecer. Escreva um algoritmo para ler o preço do litro da gasolina e o valor do pagamento,
 e exibir quantos litros ele conseguiria abastecer.'''
 
