0764d5b3374371765c8482c06fdb713f23b48030
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-09-02 09:03:58 -0300

    Exercicios

--- Desafio-Salário.py
+++ Desafio-Salário.py
@@ -1,5 +1,6 @@
+'''FAÇA UM PROGRAMA QUE CALCULE AS HORAS EXTRAS DE TRABALHADOR'''
 
-salario_hr = float(input('Valor hr: '))
+salario_hr = float(input('Valor hora: '))
 hrs_mes = float(input('Horas trabalhadas no mês: '))
 salario = (((salario_hr * 8) * 5) * 4)
 
@@ -7,4 +8,4 @@ if hrs_mes <= 160:
     print('Seu salario é:', salario)
 else:
     salario += ((salario_hr * 1.5) * (hrs_mes - 160))
-    print(salario)
+    print(f'seu salário acrecido das horas extras é R${salario}')
